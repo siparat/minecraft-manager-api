dc8eb425c9f5cafe62fe346d52d2634cf0d22c20
Siparat <akimov1712@yandex.ru>
2025-07-30 16:58:47 +0300

    chore: enable cors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UnauthorizedException, UseGuards, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RoleGuard } from './guards/role.guard';
@@ -6,10 +6,22 @@ import { UserRole } from 'generated/prisma';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { CreateBuilderAccountDto } from './dto/create-builder-account.dto';
 import { UserEntity } from './entities/user.entity';
+import { Request } from 'express';
+import { AuthErrorMessages } from 'src/auth/auth.constants';
 
 @Controller('user')
 export class UserController {
 	constructor(private userService: UserService) {}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('info')
+	getInfo(@Req() req: Request): Omit<UserEntity, 'password' | 'setPassword' | 'comparePassword'> {
+		if (!req.user) {
+			throw new UnauthorizedException(AuthErrorMessages.UNAUTHORIZED);
+		}
+		const { password, ...entity } = new UserEntity(req.user);
+		return entity;
+	}
 
 	@UseGuards(JwtAuthGuard, new RoleGuard([UserRole.ADMIN]))
 	@UsePipes(ZodValidationPipe)
