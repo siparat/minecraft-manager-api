84ffd7138d5bab4fb656c86d85c7c2c53bb06243
siparat <akimov1712@yandex.ru>
2026-02-09 10:46:42 +0300

    fix: increased the limit on the size of the object

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { getSwaggerConfig } from './configs/swagger.config';
+import { json, urlencoded } from 'express';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+	app.use(json({ limit: '50mb' }));
+	app.use(urlencoded({ extended: true, limit: '50mb' }));
 	app.enableCors();
 	app.setGlobalPrefix('v1');
 
