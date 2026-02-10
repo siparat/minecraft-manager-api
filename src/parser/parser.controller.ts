f8ac2e6b38249e8e72cb857bcaabe415bca42e49
siparat <akimov1712@yandex.ru>
2026-02-10 15:36:53 +0300

    refactor: remove start parser endpoint

--- src/parser/parser.controller.ts
+++ src/parser/parser.controller.ts
@@ -1,27 +1,4 @@
-import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
-import { ParserGateway } from './parser.gateway';
-import { ParserService } from './parser.service';
-import { ModRepository } from 'src/mod/repositories/mod.repository';
-import { CronExpression, Cron } from '@nestjs/schedule';
-import { ParserSaga } from './sagas/parser/parser.saga';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { RoleGuard } from 'src/user/guards/role.guard';
-import { UserRole } from 'generated/prisma';
+import { Controller } from '@nestjs/common';
 
 @Controller('parser')
-export class ParserController {
-	constructor(
-		private parserGateway: ParserGateway,
-		private parserService: ParserService,
-		private modRepository: ModRepository
-	) {}
-
-	@HttpCode(HttpStatus.OK)
-	@UseGuards(JwtAuthGuard, new RoleGuard([UserRole.ADMIN]))
-	@Post()
-	@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
-	async startParser(): Promise<void> {
-		const saga = new ParserSaga(this.parserGateway, this.parserService, this.modRepository);
-		saga.state.start();
-	}
-}
+export class ParserController {}
